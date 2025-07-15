696e60f3138e7a0636ce999c9697293e0ee8b2dd
Ali Elmorsy <ali.elmorsy19@gmail.com>
2025-07-15 05:07:18 +0300

    - Fix the base model

--- src/synexis/Synexis.cpp
+++ src/synexis/Synexis.cpp
@@ -454,7 +454,6 @@ void Synexis::updateLoop() {
 void Synexis::batch_add(llama_batch &batch, llama_token tokenID, int32_t nPast,
                         const std::vector<llama_seq_id> &seq_ids,
                         bool logits) {
-    batch.n_tokens = 0;
     batch.token[batch.n_tokens] = tokenID;
     batch.pos[batch.n_tokens] = nPast;
     batch.n_seq_id[batch.n_tokens] = seq_ids.size();
